dcf9328b0dcabf982d40a3bb7fb26a49fba731bf
FachowaGosiav2 <gracjan.broniecki@gmail.com>
2023-07-23 22:30:18 +0200

    fit not work

---
+++ import_train_test.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+class Import_path_to_data:
+    def Import_train_test():
+        train = Path('D:\AI\\360\Training')
+        test = Path('D:\AI\\360\Test')
+        return train, test
